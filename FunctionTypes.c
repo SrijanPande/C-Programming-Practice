136b1a2d1835047c2885b0e27ac59b20365785db
Srijan Pande <srijanpande2005@gmail.com>
2025-09-08 20:05:50 +0530

    Theory about Function Types

---
+++ FunctionTypes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+Function types are derived types based on the return type of the function as well as the number and types of parameters it accepts. 
+Do not declare functions in C with an empty parameter list.
+
+When declaring a function, a function declarator is used to specify the name of the function and the return type. If the declarator includes a parameter type list and a definition, the declaration of each parameter must include an identifier, except parameter lists with only a single parameter of the type void which requires no identifier.
+
+A function type with a parameter type list is known as a function prototype. A function prototype informs the compiler about the number and types of parameters a function accepts. 
+Compilers use this information to verify that the correct number and types of parameters are used in the function definition as well as any calls to the function.
+
+The function definition provides the actual implementation of the function
+*/
+
+int main(int argc, char *argv[]){
+    return EXIT_SUCCESS;
+}
